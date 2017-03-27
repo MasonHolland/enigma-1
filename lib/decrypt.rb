e61f9d8197d0d9ef8cdfb60bc0cdb80212e78f5f
MasonHolland <masontholland@gmail.com>
2017-03-27 15:25:33 -0600

    refactored decrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,23 +1,19 @@
 class Decrypt
   attr_reader :message
 
-    def initialize(message, cipher)
-      @message = unlock(message, cipher)
-    end
-
-    def unlock(message, cipher)
-      cipher.flip
-      unlocked_message = ""
-      i = 0
-      message.each_char do |char|
-      unlocked_message << cipher.char_map[i][char]
-      i += 1
-      i = 0 if i > 3
-      end
-      unlocked_message
-    end
-
-
-
-
+  def initialize(message, cipher)
+    @message = unlock(message, cipher)
   end
+
+  def unlock(message, cipher)
+    cipher.flip
+    unlocked_message = ""
+    i = 0
+    message.each_char do |char|
+      unlocked_message << cipher.char_map[i][char]
+      i < 3 ? i += 1: i = 0
+    end
+    unlocked_message
+  end
+
+end
